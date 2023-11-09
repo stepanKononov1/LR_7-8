523cf8458f12eef0c61aace955b0962ef80dfdc0
stepanKononov1 <94189109+stepanKononov1@users.noreply.github.com>
2023-11-09 15:01:37 +0300

    Add files via upload

--- z13.pas
+++ z13.pas
@@ -13,6 +13,10 @@ begin
     if s[i] = 'b' then b:=True;
     if s[i] = 'c' then c:=True;
   end;
-  if (a = True) and (b = True) and (c = True) then writeln('Строка содержит символы: "а", "b", "c"')
-  else writeln('Строка не содержит символы: "а", "b", "c"');
+  if a = True then writeln('Строка содержит символ "а"')
+  else writeln('Строка не содержит символ "а"');
+  if b = True then writeln('Строка содержит символ "b"')
+  else writeln('Строка не содержит символ "b"');
+  if c = True then writeln('Строка содержит символ "c"')
+  else writeln('Строка не содержит символ "c"');
 end.
